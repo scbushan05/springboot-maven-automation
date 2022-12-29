97bcea01bbc65e257d1846485d2fc4e5c47ba23d
Bushan SC <bushan1992@zohomail.in>
2022-12-29 22:06:21 +0530

    initial commit

---
+++ src/main/java/in/bushansirgur/githubautomation/GithubAutomationApplication.java
@@ -0,0 +1,13 @@
+package in.bushansirgur.githubautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubAutomationApplication.class, args);
+	}
+
+}
